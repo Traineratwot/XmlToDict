f8df971653829a06a6ce849eef6baa164bf79089
Кирилл <traineratwot@yandex.ru>
2026-02-18 22:12:52 +0300

    update

--- src/XmlToDict.php
+++ src/XmlToDict.php
@@ -8,90 +8,131 @@ use RuntimeException;
 
 class XmlToDict
 {
-	public static $keyName = '_++___arr__#_key___@_';
-	public static $counter = 0;
+    private const ARRAY_KEY = '_++___arr__#_key___@_';
+    private const TEXT_KEY = '#text';
+    private const ATTR_PREFIX = '@';
+    private const XMLNS_PREFIX = '@xmlns';
 
-	static function parse($xmlString): array
-	{
-		$xml = simplexml_load_string($xmlString);
-		return self::parseXml($xml);
-	}
+    private static int $counter = 0;
 
-	private static function parseXml(SimpleXMLElement $xml)
-	{
-		self::$counter = 0;
-		$root          = $xml->getName();
-		$content       = self::xmlToArray($xml);
-		return [
-				$root => $content,
-		];
-	}
+    /**
+     * Parse XML string to associative array
+     */
+    public static function parse(string $xmlString): array
+    {
+        $xml = simplexml_load_string($xmlString);
+        if ($xml === false) {
+            throw new RuntimeException('Failed to parse XML string');
+        }
+        return self::parseXml($xml);
+    }
 
-	static function xmlToArray(SimpleXMLElement $xml)
-	{
-		self::$counter++;
-		$array = [];
-		foreach ($xml->attributes() as $attrName => $attrValue) {
-			if (!empty($attrValue)) {
-				$array['@' . $attrName] = (string)$attrValue;
-			}
-		}
-		foreach ($xml->getDocNamespaces() as $attrName => $attrValue) {
-			if (!empty($attrValue)) {
-				if ($attrName) {
-					$array['@xmlns:' . $attrName] = (string)$attrValue;
-				} else {
-					$array['@xmlns'] = (string)$attrValue;
-				}
-			}
-		}
-		foreach ($xml->children() as $element) {
-			$name  = $element->getName();
-			$count = $element->count();
-			$data  = [];
-			// Add attributes to the data array
-			$attributes = $element->attributes();
-			foreach ($attributes as $attrName => $attrValue) {
-				$data['@' . $attrName] = (string)$attrValue;
-			}
-			if ($element->count() === 0) {
-				if ((string)$element) {
-					$data['#text'] = (string)$element;
-				}
-			} else {
-				$data = self::xmlToArray($element);
-			}
+    /**
+     * Load and parse XML file
+     */
+    public static function load(string $filename): array
+    {
+        if (!file_exists($filename)) {
+            throw new RuntimeException("File not found: {$filename}");
+        }
+        $xml = simplexml_load_file($filename);
+        if ($xml === false) {
+            throw new RuntimeException("Failed to parse XML file: {$filename}");
+        }
+        return self::parseXml($xml);
+    }
 
-			if (is_array($data) and count($data) === 1 and array_key_exists('#text', $data)) {
-				$data = $data['#text'];
-			}
-			if (is_array($data) and empty($data)) {
-				$data = NULL;
-			}
-			if (is_array($data) && array_key_exists(self::$keyName, $data)) {
-				$key = $data[self::$keyName];
-				unset($data[self::$keyName]);
-				$array[$name][$key] = $data;
-			} else {
-				$array[$name][] = $data;
-			}
+    /**
+     * Parse SimpleXMLElement to array
+     */
+    private static function parseXml(SimpleXMLElement $xml): array
+    {
+        self::$counter = 0;
+        $root = $xml->getName();
+        $content = self::xmlToArray($xml);
+        return [$root => $content];
+    }
 
-		}
-		foreach ($array as $key => $value) {
-			if (is_array($value) && count($value) === 1) {
-				if (array_key_exists(0, $value)) {
-					$array[$key] = $value[0];
-				} else if (array_key_exists('#text', $value)) {
-					$array[$key] = $value['#text'];
-				}
-			}
-		}
-		return $array;
-	}
+    /**
+     * Convert SimpleXMLElement to array recursively
+     */
+    private static function xmlToArray(SimpleXMLElement $xml): mixed
+    {
+        self::$counter++;
+        $array = [];
 
-	static function load($filename): array
-	{
-		$xml = simplexml_load_file($filename);
-		return self::parseXml($xml);
-	}
+        // Process attributes
+        foreach ($xml->attributes() as $attrName => $attrValue) {
+            if (!empty($attrValue)) {
+                $array[self::ATTR_PREFIX . $attrName] = (string)$attrValue;
+            }
+        }
+
+        // Process namespaces
+        foreach ($xml->getDocNamespaces() as $attrName => $attrValue) {
+            if (!empty($attrValue)) {
+                $key = $attrName ? self::XMLNS_PREFIX . ':' . $attrName : self::XMLNS_PREFIX;
+                $array[$key] = (string)$attrValue;
+            }
+        }
+
+        // Process child elements
+        $children = [];
+        foreach ($xml->children() as $element) {
+            $name = $element->getName();
+            $children[$name][] = $element;
+        }
+
+        foreach ($children as $name => $elements) {
+            if (count($elements) === 1) {
+                // Single element - process normally
+                $array[$name] = self::processElement($elements[0]);
+            } else {
+                // Multiple elements - always create array
+                $array[$name] = array_map(
+                    fn(SimpleXMLElement $el) => self::processElement($el),
+                    $elements
+                );
+            }
+        }
+
+        return $array;
+    }
+
+    /**
+     * Process single XML element
+     */
+    private static function processElement(SimpleXMLElement $element): mixed
+    {
+        $data = [];
+
+        // Add attributes
+        foreach ($element->attributes() as $attrName => $attrValue) {
+            $data[self::ATTR_PREFIX . $attrName] = (string)$attrValue;
+        }
+
+        // Check if element has children
+        if ($element->count() === 0) {
+            // Leaf element
+            $text = (string)$element;
+            if ($text !== '') {
+                $data[self::TEXT_KEY] = $text;
+            }
+        } else {
+            // Element with children - recurse
+            $data = self::xmlToArray($element);
+        }
+
+        // Simplify single text-only elements
+        if (count($data) === 1 && array_key_exists(self::TEXT_KEY, $data)) {
+            return $data[self::TEXT_KEY];
+        }
+
+        // Return null for empty elements
+        if (empty($data)) {
+            return null;
+        }
+
+        return $data;
+    }
 }
